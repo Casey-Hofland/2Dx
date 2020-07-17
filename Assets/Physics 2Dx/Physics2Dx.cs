4ee36dbfc3502ce061a8f09130196ffba386463b
Casey Hofland <hofland.casey@gmail.com>
2020-07-17 21:30:14 +0200

    Initial commit

---
+++ Assets/Physics 2Dx/Physics2Dx.cs
@@ -0,0 +1,168 @@
+﻿using Physics2DxSystem;
+using System;
+using System.Collections;
+
+namespace UnityEngine
+{
+    public class Physics2Dx
+    {
+        internal const string componentMenu = "Physics 2Dx/";
+
+        #region Persistent
+        private static Persistent coroutineHandler;
+
+        private class Persistent : MonoBehaviour
+        {
+            public static bool willDisable = false;
+
+            public static void Init()
+            {
+                willDisable = false;
+
+                Application.quitting -= SetCanDisableToTrue;
+                Application.quitting += SetCanDisableToTrue;
+            }
+
+            private static void SetCanDisableToTrue()
+            {
+                willDisable = true;
+            }
+
+            private void OnDisable()
+            {
+                if(!willDisable)
+                {
+                    throw new CannotDisableException($"{GetType().Name} may not be disabled except on quit!");
+                }
+            }
+
+            private class CannotDisableException : Exception
+            {
+                public CannotDisableException() { }
+                public CannotDisableException(string message) : base(message) { }
+                public CannotDisableException(string message, Exception innerException) : base(message, innerException) { }
+            }
+        }
+
+        public static Coroutine StartCoroutine(IEnumerator routine) => Persistent.willDisable ? null : coroutineHandler.StartCoroutine(routine);
+        public static void StopCoroutine(Coroutine routine)
+        {
+            if(!Persistent.willDisable)
+            {
+                coroutineHandler.StopCoroutine(routine);
+            }
+        }
+        public static void StopCoroutine(IEnumerator routine)
+        {
+            if(!Persistent.willDisable)
+            {
+                coroutineHandler.StopCoroutine(routine);
+            }
+        }
+        public static void StopAllCoroutines()
+        {
+            if(!Persistent.willDisable)
+            {
+                coroutineHandler.StopAllCoroutines();
+            }
+        }
+        #endregion
+
+        #region Conversion
+        private static bool _is2Dnot3D = false;
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/is2Dnot3D/*' />
+        public static bool is2Dnot3D
+        {
+            get => _is2Dnot3D;
+            set
+            {
+                if(isConverting || _is2Dnot3D == value)
+                {
+                    return;
+                }
+
+                StartCoroutine(UpdateIsConverting(value));
+                if(splitConversionOverMultipleFrames)
+                {
+                    StartCoroutine(Module2Dx.ConvertInstancesAsCoroutine(value));
+                }
+                else
+                {
+                    Module2Dx.ConvertInstances(value);
+                }
+            }
+        }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/Convert/*' />
+        public static void Convert()
+        {
+            is2Dnot3D = !is2Dnot3D;
+        }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/onConvert/*' />
+        public delegate void OnConvert(bool to2Dnot3D);
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/onConvert/*' />
+        public static OnConvert onBeforeConvert { get; set; }
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/onConvert/*' />
+        public static OnConvert onAfterConvert { get; set; }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/waitForConversionTime/*' />
+        public static WaitForSecondsRealtime waitForConversionTime { get; private set; }
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/conversionTime/*' />
+        public static float conversionTime
+        {
+            get => waitForConversionTime.waitTime;
+            set => waitForConversionTime.waitTime = value;
+        }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/isConverting/*' />
+        public static bool isConverting { get; private set; }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/waitWhileConverting/*' />
+        public static WaitWhile waitWhileConverting => new WaitWhile(IsConverting);
+
+        private static bool IsConverting() => isConverting;
+
+        private static IEnumerator UpdateIsConverting(bool to2Dnot3D)
+        {
+            onBeforeConvert?.Invoke(to2Dnot3D);
+
+            isConverting = true;
+            yield return waitForConversionTime;
+            isConverting = false;
+
+            _is2Dnot3D = to2Dnot3D;
+            onAfterConvert?.Invoke(to2Dnot3D);
+        }
+
+        /// <include file='./Documentation.xml' path='docs/Physics2Dx/splitConversionOverMultipleFrames/*' />
+        public static bool splitConversionOverMultipleFrames { get; set; }
+        #endregion
+
+        #region Debugging
+        public static bool slimHierarchy { get; private set; }
+        #endregion
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        private static void Init()
+        {
+            onBeforeConvert = onAfterConvert = default;
+
+            var settings = Resources.Load<Physics2DxSettings>(nameof(Physics2DxSettings));
+            _is2Dnot3D = settings.is2Dnot3D;
+            waitForConversionTime = new WaitForSecondsRealtime(settings.conversionTime);
+            splitConversionOverMultipleFrames = settings.splitConversionOverMultipleFrames;
+            slimHierarchy = settings.slimHierarchy;
+
+            var coroutineHandlerGO = new GameObject(nameof(coroutineHandler));
+            Object.DontDestroyOnLoad(coroutineHandlerGO);
+            if(slimHierarchy)
+            {
+                coroutineHandlerGO.hideFlags |= HideFlags.HideInHierarchy;
+            }
+
+            coroutineHandler = coroutineHandlerGO.AddComponent<Persistent>();
+            Persistent.Init();
+        }
+    }
+}
---
+++ Assets/Physics 2Dx/Utilities/RuntimeComponentCopier.cs
@@ -0,0 +1,86 @@
+﻿// Modified from an implementation by vexe.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Physics2DxSystem.Utilities
+{
+    public static class RuntimeComponentCopier
+    {
+        private const BindingFlags bindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+
+        /// <summary>Gets a Copy of another Component</summary>
+        /// <param name="comp">The Component to copy to.</param>
+        /// <param name="other">The Component to copy from.</param>
+        /// <param name="ignore">The properties to ignore when copying.</param>
+        /// <returns>The new Component.</returns>
+        public static T GetCopyOf<T>(this Component comp, T other, params string[] ignore) where T : Component
+        {
+            Type type = comp.GetType();
+            if(type != other.GetType()) return null; // type mis-match
+
+            Type derivedType = typeof(T);
+            bool hasDerivedType = derivedType != type;
+
+            IEnumerable<PropertyInfo> pinfos = type.GetProperties(bindingFlags);
+            if(hasDerivedType)
+            {
+                pinfos = pinfos.Concat(derivedType.GetProperties(bindingFlags));
+            }
+            pinfos = from property in pinfos
+                     where !property.CustomAttributes.Any(attribute => attribute.AttributeType == typeof(ObsoleteAttribute))
+                     where Array.IndexOf(ignore, property.Name) == -1
+                     select property;
+            foreach(var pinfo in pinfos)
+            {
+                if(pinfo.CanWrite)
+                {
+                    if(pinfos.Any(e => e.Name == $"shared{char.ToUpper(pinfo.Name[0])}{pinfo.Name.Substring(1)}")) // Make sure that properties which have a "shared..." value are not copied to prevent leaking objects.
+                    {
+                        continue;
+                    }
+
+                    try
+                    {
+                        pinfo.SetValue(comp, pinfo.GetValue(other));
+                    }
+                    catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, but not specifying anything does catch it.
+                }
+            }
+
+            IEnumerable<FieldInfo> finfos = type.GetFields(bindingFlags);
+            if(hasDerivedType)
+            {
+                finfos = finfos.Concat(derivedType.GetFields(bindingFlags));
+            }
+            finfos = from field in finfos
+                     where field.CustomAttributes.Any(attribute => attribute.AttributeType == typeof(ObsoleteAttribute))
+                     where !ignore.Contains(field.Name)
+                     select field;
+            foreach(var finfo in finfos)
+            {
+                if(finfos.Any(e => e.Name == $"shared{char.ToUpper(finfo.Name[0])}{finfo.Name.Substring(1)}")) // Make sure that fields which have a "shared..." value are not copied to prevent leaking objects.
+                {
+                    continue;
+                }
+
+                finfo.SetValue(comp, finfo.GetValue(other));
+            }
+
+            return comp as T;
+        }
+
+        /// <summary>Add a copy of a Component to a GameObject.</summary>
+        /// <param name="toAdd">The Component to copy from.</param>
+        /// <param name="ignore">The properties to ignore when copying.</param>
+        /// <returns>The new Component.</returns>
+        public static T AddComponent<T>(this GameObject go, T toAdd, params string[] ignore) where T : Component
+        {
+            return go.AddComponent(toAdd.GetType()).GetCopyOf(toAdd, ignore);
+        }
+    }
+}
+
