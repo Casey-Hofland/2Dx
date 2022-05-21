a5dc1be6359a607d1dcae35b65be3d520137716c
CaseyDeCoder <hofland.casey@gmail.com>
2022-05-21 09:05:35 +0200

    Decouple the ConvertibleSwitchDrawer from the ConvertibleSwitchAttribute script

--- Runtime/Core/Converters/ConvertibleSwitchAttribute.cs
+++ Runtime/Core/Converters/ConvertibleSwitchAttribute.cs
@@ -1,28 +1,6 @@
 using UnityEngine;
-using UnityEditor;
 
 namespace Unity2Dx
 {
     internal class ConvertibleSwitchAttribute : PropertyAttribute { }
 }
-
-#if UNITY_EDITOR
-namespace Unity2Dx.Editor
-{
-    [CustomPropertyDrawer(typeof(ConvertibleSwitchAttribute))]
-    public class ConvertibleSwitchDrawer : PropertyDrawer
-    {
-        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-        {
-            EditorGUI.BeginChangeCheck();
-            var newValue = EditorGUI.Toggle(position, label, property.boolValue);
-            if (EditorGUI.EndChangeCheck())
-            {
-                Undo.RecordObject(property.serializedObject.targetObject, "Convertible Switch");
-                var c = property.serializedObject.targetObject as IConvertible;
-                c.Convert(newValue);
-            }
-        }
-    }
-}
-#endif
